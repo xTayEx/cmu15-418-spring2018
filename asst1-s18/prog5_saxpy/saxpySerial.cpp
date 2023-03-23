f2b0390d63543ea4002926f4f1c764c9090640ea
xtayex <75450574@qq.com>
2023-03-23 18:46:28 +0800

    in prog5, compilation failed if using _mm_stream_load_si128

--- asst1-s18/prog5_saxpy/saxpySerial.cpp
+++ asst1-s18/prog5_saxpy/saxpySerial.cpp
@@ -1,13 +1,6 @@
+void saxpySerial(int N, float scale, float X[], float Y[], float result[]) {
 
-void saxpySerial(int N,
-                       float scale,
-                       float X[],
-                       float Y[],
-                       float result[])
-{
-
-    for (int i=0; i<N; i++) {
-        result[i] = scale * X[i] + Y[i];
-    }
+  for (int i = 0; i < N; i++) {
+    result[i] = scale * X[i] + Y[i];
+  }
 }
-
--- asst1-s18/prog5_saxpy/saxpyStreaming.cpp
+++ asst1-s18/prog5_saxpy/saxpyStreaming.cpp
@@ -1,22 +1,21 @@
-#include <smmintrin.h> // For _mm_stream_load_si128
-#include <emmintrin.h> // For _mm_mul_ps
 #include <assert.h>
+#include <immintrin.h>
+#include <emmintrin.h> // For _mm_mul_ps
+#include <smmintrin.h> // For _mm_stream_load_si128
 #include <stdint.h>
+#include <cstdlib>
+#include <xmmintrin.h>
 
-extern void saxpySerial(int N,
-			float scale,
-			float X[],
-			float Y[],
-			float result[]);
+extern void saxpySerial(int N, float scale, float X[], float Y[],
+                        float result[]);
 
-
-void saxpyStreaming(int N,
-                    float scale,
-                    float X[],
-                    float Y[],
-                    float result[])
-{
-    // Replace this code with ones that make use of the streaming instructions
-    saxpySerial(N, scale, X, Y, result);
+void saxpyStreaming(int N, float scale, float X[], float Y[], float result[]) {
+  // Replace this code with ones that make use of the streaming instructions
+  __m128 x, y;
+  __m128 s = _mm_set1_ps(scale);
+  for (int i = 0; i < N; i += 4) {
+    x = _mm_load_ps(X + i);
+    y = _mm_load_ps(Y + i);
+    _mm_store_ps(result + i, _mm_add_ps(_mm_mul_ps(s, x), y));
+  }
 }
-
